419761d94683b97b73a29b83ce80282310b50e8e
WONDongin <bangsa100@naver.com>
2025-09-03 23:43:56 +0900

    feat:[A-B]
    첫째 줄에 A-B를 출력 + .gitignore 파일추가

---
+++ baekjoon/no_2557/No2557.java
@@ -0,0 +1,10 @@
+package no_2557;
+/*
+문제 : 서울의 오늘 날짜를 출력하는 프로그램을 작성하시오.
+출력 : 서울의 오늘 날짜를 "YYYY-MM-DD" 형식으로 출력한다.
+*/
+public class No2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
