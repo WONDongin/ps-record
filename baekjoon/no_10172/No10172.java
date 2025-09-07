2bfb4b311ffd5f68a119122a42f1c393dc603108
WONDongin <bangsa100@naver.com>
2025-09-07 22:50:44 +0900

    feat:[개,고양이,새싹 출력]
    개,고양이,새싹 출력 (\",\\,"^,"`)

---
+++ baekjoon/no_10172/No10172.java
@@ -0,0 +1,11 @@
+package no_10172;
+// 문제 : 개를 출력한다.
+public class No10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
---
+++ baekjoon/no_25083/No25083.java
@@ -0,0 +1,12 @@
+package no_25083;
+// 문제 : 새싹를 출력한다.
+public class No25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
