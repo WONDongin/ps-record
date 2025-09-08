ff3e70d7daf2336786674eabec8f0c288d9be0e4
WONDongin <bangsa100@naver.com>
2025-09-08 19:41:47 +0900

    feat:[9,10]
    문자열 붙여서 출력 + %d

---
+++ programmers/level0/No10.java
@@ -0,0 +1,12 @@
+package level0;
+
+import java.util.Scanner;
+// 문제 : 문자열 붙여서 출력하기
+public class No10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a + b);
+    }
+}
