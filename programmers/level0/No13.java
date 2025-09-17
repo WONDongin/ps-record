2eb7f10abc0422f2f0813551d0d01bfd1565acb6
WONDongin <bangsa100@naver.com>
2025-09-17 23:33:03 +0900

    feat:[숫자비교하기]
    정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.

---
+++ programmers/level0/No13.java
@@ -0,0 +1,13 @@
+package level0;
+// 문제 : 정수 num1과 num2 두 수가 같으면 1 다르면 -1을 retrun
+public class No13 {
+    public int solution(int num1, int num2){
+        return (num1 == num2) ? 1 : -1;
+        // if(num1 == num2){
+        //     answer = 1;
+        // } else {
+        //     answer = -1;
+        // }
+        // return answer;
+    }
+}
