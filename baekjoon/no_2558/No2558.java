ad7eee12209d22fe916f74e9c2fc8a38a9a8d4bb
WONDongin <bangsa100@naver.com>
2025-09-27 23:44:44 +0900

    feat:[4153/2558]
    직각삼각형
    - 각 입력에 대해 직각 삼각형이 맞다면 "right", 아니라면 "wrong"을 출력

---
+++ baekjoon/no_2558/No2558.java
@@ -0,0 +1,12 @@
+package no_2558;
+
+import java.util.Scanner;
+// 문제 : A+B - 2 (첫째 줄에 A+B를 출력)
+public class No2558 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
