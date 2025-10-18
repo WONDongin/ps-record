b88d8951d4fc5672de1db889cf3a2ace218da92d
WONDongin <bangsa100@naver.com>
2025-10-19 00:06:28 +0900

    refactor:[35]
    문제 : a, b 사이의 모든수의 합
    
    가우스의 합 공식 활용
    합 =((end−start+1)×(start+end)) / 2
    
    Co-Authored-By: WDIDB <161272670+WDIDB@users.noreply.github.com>

--- programmers/level1/No10.java
+++ programmers/level1/No10.java
@@ -4,23 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+/*
+문제 : a, b 사이의 모든수의 합
 
+가우스의 합 공식 활용
+합 =((end−start+1)×(start+end)) / 2
+*/
 public class No10 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(br.readLine());
         int b = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[2];
-        arr[0] = a;
-        arr[1] = b;
-        Arrays.sort(arr);
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
 
-        int sum = 0;
-        for(int i = arr[0]; i <= arr[1]; i++){
-            sum += i;
-        }
-
-        System.out.println(sum);
+        System.out.print((end - start +1) * (start + end) / 2);
     }
 }
