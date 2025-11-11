7fa2ab01dcd52455202984f23f367969bb121c5e
원동인 (Dongin Won) <88768560+WONDongin@users.noreply.github.com>
2025-11-11 18:41:38 +0900

    feat:[2393]
    문제 : Rook
    출력 : 탑모양 출력

---
+++ baekjoon_java/no_2393/2393.java
@@ -0,0 +1,22 @@
+package no_2393;
+/*
+문제 : Rook
+출력 : 탑모양 출력
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+    }
+}
